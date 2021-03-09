9a655d7ae2db1d5fdd6fc0917b824cf6b9c40403
Bruno Medeiros <bruno.am19@gmail.com>
2021-03-09 15:10:57 +0400

    Lesson 1 - 6

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
-      product: "Socks",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, odio?",
+      cart: 0,
+      product: 'Socks',
+      image: './assets/images/socks_blue.jpg',
+      inStock: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+      ],
     };
   },
 });
