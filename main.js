0c76519f291ccd6dd80f3666f0afd1d71c44626c
Bruno Medeiros <bruno.am19@gmail.com>
2021-03-09 11:02:38 +0400

    Creating the Vue App

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, odio?",
+    };
+  },
+});
